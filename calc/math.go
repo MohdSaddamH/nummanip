4ae553e813088e30edfb2df7b1f5c3cb495f5ff2
root <root@style-centos7.chetu.com>
2020-09-22 17:00:04 +0530

    Patch commit for calc package

--- calc/math.go
+++ calc/math.go
@@ -3,6 +3,12 @@ package calc
 /*** Add two number 
 * return int
 */
-func Add(i int, j int) int{
-	return i + j
-}
+func Add(numbers ...int) int{
+	
+	var sum int = 0
+	for _, num := range numbers{
+		sum += num
+	}
+	
+	return sum
+}	
